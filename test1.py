e7213aae8a7f4af9581008c4a24b6f2c1eb36d4c
1601 <1243153506@qq.com>
2018-09-03 19:30:17 +0800

    asdfads

--- test1.py
+++ test1.py
@@ -1,5 +1,10 @@
 print("hello github")
 print("chenfazhan")
 
+2018.9.3
+安装了git,练习了git命令
+成功安装，并且能成功运行，小组的修改文件也已成功	
+安装gow后，linux命令还是无法成功	
+对git命令进一步加深印象，并且对明天的课程进度，更近一步的加深，提高自己的效率
 
 print("nishishagua")
