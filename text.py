6434fc03e84c13c8381642d73b79a0b3f94eae77
pyb2315 <1399671033@qq.com>
2018-09-03 19:41:12 +0800

    彭奕博修改

--- text.py
+++ text.py
@@ -1,4 +1,4 @@
-print('from lixuhui that is wanghai change again more again')
+﻿print('from lixuhui that is wanghai change again more again')
 print('李旭辉来了！！')
 <<<<<<< HEAD
 print('再一asdfasdfadsfas')
@@ -6,3 +6,5 @@ print('再一asdfasdfadsfas')
 print('再一次进行修改')
 print('杨豪康修改记录')
 >>>>>>> a6d36c209d9eaf83b32d3dac14df34a3425825f3
+print（'彭奕博在来一次'）
+print（'告辞!!!'）
